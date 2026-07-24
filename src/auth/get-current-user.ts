ec5fb07ec98347a457243e57ad6a7461feb677fe
adub20018 <alex.w.malone128@gmail.com>
2026-07-24 23:27:18 +1200

    removed redundant logs

--- src/auth/get-current-user.ts
+++ src/auth/get-current-user.ts
@@ -1,11 +1,13 @@
 import { cache } from "react";
 import { auth } from "@/auth/server";
+import { redirect } from "next/navigation";
 
 export const getCurrentUser = cache(async () => {
   const { data: session } = await auth.getSession();
 
   if (!session) {
-    throw new Error("Unauthenticated user");
+    console.log("Not logged in. Redirecting to login...");
+    redirect("/login");
   }
 
   return session.user;
--- src/auth/server.ts
+++ src/auth/server.ts
@@ -1,8 +1,5 @@
 import { createNeonAuth } from "@neondatabase/auth/next/server";
 
-console.log("NEON AUTH URL:", process.env.NEON_AUTH_BASE_URL);
-console.log("COOKIE SECRET EXISTS:", !!process.env.NEON_AUTH_COOKIE_SECRET);
-
 export const auth = createNeonAuth({
   baseUrl: process.env.NEON_AUTH_BASE_URL!,
   cookies: {
